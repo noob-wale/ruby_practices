a64c812a85bc6b891cc099dfa10a3f3825f84542
noob-wale <wallacesheundah@gmail.com>
2023-05-24 01:52:21 +0300

    updaated hhello.rbb

--- hello.rb
+++ hello.rb
@@ -3,6 +3,31 @@
 # puts 'I like typing this.'
 # puts 'this is fun'
 
+# numbers
+
+# puts "I will now count my chhickens."
+
+# puts "Hens #{25+30/6}"
+# puts "Roosters #{100-25 * 3 % 4}"
+
+# puts "Noww i will count the eggs:"
+
+# puts 3+2+1-5+4%2-1/4+6
+
+# puts "Is it true thaat 3 + 2 < 5 - 7?"
+
+# puts 3 + 2 < 5 - 7
+
+# puts "what is 3 + 2? #{3+2}"
+# puts "What is 5 - 7? #{5-7}"
+
+# puts "ohh thats why its false."
+
+# puts "how about some more."
+
+# puts "is it greater? #{5> -2}"
+# puts "is it greater or equal? #{5>= -2}"
+# puts "is it less or equal? #{5 <= -2}"
 
 
 # optional_parameters.rb
